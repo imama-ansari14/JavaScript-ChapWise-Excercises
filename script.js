77f7ade628b2618704f2fd9a9932977bd3d02f5c
Imama Ansari <ansariimama@yahoo.com>
2025-03-11 14:48:45 +0500

    chap 1 four questions solved

---
+++ script.js
@@ -0,0 +1,11 @@
+//--------- CHAP NO 1----------
+// QUESTION NO 1 
+alert("Hello, I hope your day is going good, How may i help you?")
+// QUESTION NO 2
+alert("Error! Please enter a valid password")
+// QUESTION NO 3
+alert("Welcome to the JS Land.. \n Happy Coding..!")
+// QUESTION NO 4 ..
+alert("\n \n Welcome to JS Land")
+alert("Happy Coding!") 
+// QUESTION NO 5

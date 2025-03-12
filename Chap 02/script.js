4ec12a44771c5ed53fb38027c1c665a28edaa0c9
Imama Ansari <ansariimama@yahoo.com>
2025-03-12 23:40:56 +0500

    JavaScript Chap 02 completed

---
+++ Chap 02/script.js
@@ -0,0 +1,31 @@
+// QUESTION NO 1
+var username;
+// QUESTION NO 2
+var myname ="Imama Ansari"
+document.write(myname)
+// QUESTION NO 3
+var message ;
+message ="Hello World";
+alert(message);
+// QUESTION NO 4
+var stName= "jhone doe"
+var age="15 years old"
+var domain="certified mobile application development"
+alert(stName)
+alert(age)
+alert(domain)
+// QUESTION NO 5
+var food="PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(food)
+// QUESTION NO 6
+var email="ansariimama@yahoo.com"
+alert("my email address is " +email )
+// QUESTION NO 7
+var book ="A smarter way to learn JavaScript"
+alert("i am trying to learn from the book "+book )
+// QUESTION NO 8
+document.write("Yah1 i can write HTML content throught JavaScript.")
+// QUESTION NO 9
+var string="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(string);
+document.write(string)

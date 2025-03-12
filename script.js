5274ef128b0ca6e1ee08890d531c211d8d9a8dac
Imama Ansari <ansariimama@yahoo.com>
2025-03-12 07:39:19 +0500

    JS Chap 1 successfully completed

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //--------- CHAP NO 1----------
 // QUESTION NO 1 
-alert("Hello, I hope your day is going good, How may i help you?")
+alert("Hello, I hope your day is going good, How may i help you?");
 // QUESTION NO 2
-alert("Error! Please enter a valid password")
+alert("Error! Please enter a valid password");
 // QUESTION NO 3
-alert("Welcome to the JS Land.. \n Happy Coding..!")
-// QUESTION NO 4 ..
-alert("\n \n Welcome to JS Land")
-alert("Happy Coding!") 
+alert("Welcome to the JS Land.. \n Happy Coding..!");
+// QUESTION NO 4
+alert("\n \n Welcome to JS Land");
+alert("Happy Coding! \n Prevent this page from creating additional dialogues.");
 // QUESTION NO 5
+alert("Hello... I can run JS through my web browser's console");
+console.log("Hello... I can run JS through my web browser's console");
+// QUESTION NO 6
+// used inside the QUESTION NO 7 by making various alerts 

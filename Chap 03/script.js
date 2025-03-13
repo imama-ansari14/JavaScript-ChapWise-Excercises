32648683e7e2eee29abf096136213ab475f24a77
Imama Ansari <ansariimama@yahoo.com>
2025-03-13 17:38:33 +0500

    JavaScript Chap 03 completed

---
+++ Chap 03/script.js
@@ -0,0 +1,15 @@
+// QUESTION NO 1
+var age;
+age=18;
+alert("I am "+ age+" years old");
+// QUESTION NO 2
+var tracker ="You have visited this site 14 times"
+alert(tracker)
+// QUESTION NO 3
+var birthYear ="2007"
+document.write("My birth year is "+ birthYear+ "<br> Data type of my declared variable is number")
+// QUESTION NO 4
+var visitorName="John Doe"
+var productTitle="T-Shirts"
+var quantity=5
+document.write(visitorName+" ordered "+quantity+" "+productTitle+" on www.xyzClothing.com")

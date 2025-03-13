02c3f90ed97ed85f522b91662869ec6749ba8e13
Imama Ansari <ansariimama@yahoo.com>
2025-03-13 22:15:40 +0500

    2 Questions of Chap 4 DONE....

---
+++ Chap 04/script.js
@@ -0,0 +1,11 @@
+// QUESTION NO 1
+var name , city , country 
+// QUESTION NO 2
+var stname="imama"
+var collage="PECHS"
+var age="18"
+var qualification ="inter"
+var residence ="FB Area"
+
+var
+// QUESTION NO 3

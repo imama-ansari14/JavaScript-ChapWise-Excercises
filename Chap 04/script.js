2464b3b0ca9b8c64420e8cdbe65ecbfee026968e
Imama Ansari <ansariimama@yahoo.com>
2025-03-14 21:58:10 +0500

    JavaScript Chap 4 completed...

--- Chap 04/script.js
+++ Chap 04/script.js
@@ -7,5 +7,26 @@ var age="18"
 var qualification ="inter"
 var residence ="FB Area"
 
-var
+var 4name 
+var *class
+var full name
+var 3Product
+var sm#it
+
 // QUESTION NO 3
+document.write("<h1>Rules for naming JS Variables</h1>")
+var no="numbers"
+var letter="letter"
+var dollar="$"
+var underScore="_"
+var jsCase="sensitive"
+var keyword="keywords"
+
+document.write("Variables names can only contain,"+ no+ " , " + dollar+" and "+ underScore+"."+ "For Example:" +dollar+ "my"+underScore+"1st Variable")
+
+document.write("<br>"+"Variable must begin with a "+letter+ ", "+dollar+ " or "+underScore+"."+
+"For Example : "+dollar+ "name,"+underScore+"name or name")
+
+document.write("<br>"+"Variable names are case "+jsCase )
+
+document.write("<br>"+"Variable names should not be JS "+ keyword)

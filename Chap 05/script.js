7e79c0946ce2496557c289abafd1185a9f041f62
Imama Ansari <ansariimama@yahoo.com>
2025-03-21 23:50:21 +0500

    Question no 1 of chap 5 is done

--- Chap 05/script.js
+++ Chap 05/script.js
@@ -1,1 +1,6 @@
 // QUESTION NO 1
+let num1 =+prompt("Enter your any frav number")
+let num2 =+prompt("Enter your another frav number")
+
+let result=num1+num2
+document.write('Sum of '+num1+' and '+num2+' is '+result)

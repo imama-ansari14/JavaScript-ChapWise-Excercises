387beba0fab7d1f15776e54ff9b61c3ee5640cdb
Imama Ansari <ansariimama@yahoo.com>
2025-03-22 00:19:27 +0500

    CHAP 5 is in process...

--- Chap 05/script.js
+++ Chap 05/script.js
@@ -4,3 +4,19 @@ let num2 =+prompt("Enter your another frav number")
 
 let result=num1+num2
 document.write('Sum of '+num1+' and '+num2+' is '+result)
+
+// QUESTION NO 2
+let sub=num1-num2;
+let mul=num1*num2;
+let div=num1/num2;
+let mod=num1%num2;
+
+document.write("Addition:"+sub+ "<br>")
+document.write("Multiplication:"+mul+ "<br>")
+document.write("Division:"+div+ "<br>")
+document.write("Modulus:"+mod+ "<br>")
+
+// QUESTION NO 5
+let table=4
+
+document.write("Table of 4 <br>"+table+"x1="+table)

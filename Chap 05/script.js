d78aa92e96834a53104c8f2e739f8c431ca7456d
Imama Ansari <ansariimama@yahoo.com>
2025-03-22 22:28:16 +0500

    question no 3 of chap 5 is done...

--- Chap 05/script.js
+++ Chap 05/script.js
@@ -1,22 +1,44 @@
 // QUESTION NO 1
-let num1 =+prompt("Enter your any frav number")
-let num2 =+prompt("Enter your another frav number")
+// let num1 =+prompt("Enter your any frav number")
+// let num2 =+prompt("Enter your another frav number")
 
-let result=num1+num2
-document.write('Sum of '+num1+' and '+num2+' is '+result)
+// let result=num1+num2
+// document.write('Sum of '+num1+' and '+num2+' is '+result)
 
 // QUESTION NO 2
-let sub=num1-num2;
-let mul=num1*num2;
-let div=num1/num2;
-let mod=num1%num2;
+// let sub=num1-num2;
+// let mul=num1*num2;
+// let div=num1/num2;
+// let mod=num1%num2;
 
-document.write("Addition:"+sub+ "<br>")
-document.write("Multiplication:"+mul+ "<br>")
-document.write("Division:"+div+ "<br>")
-document.write("Modulus:"+mod+ "<br>")
+// document.write("Addition:"+sub+ "<br>")
+// document.write("Multiplication:"+mul+ "<br>")
+// document.write("Division:"+div+ "<br>")
+// document.write("Modulus:"+mod+ "<br>")
+
+// QUESTION NO 3
+let variable
+document.write("Value after variable declaration is "+ variable+ "<br>")
+
+variable=5
+document.write("Initial Value: "+5+ "<br>")
+
+let inc = ++variable
+document.write("Value after increment is: "+ inc + "<br>")
+
+variable2=variable+7
+document.write("Value after addition is: "+variable2 + "<br>")
+
+let dec= --variable2
+document.write("Value after decrement is:" +dec + "<br>")
+
+let div =dec%3
+document.write("The remainder is "+div)
+
+// QUESTION NO 4
+
 
 // QUESTION NO 5
-let table=4
+// let table=4
 
-document.write("Table of 4 <br>"+table+"x1="+table)
+// document.write("Table of 4 <br>"+table+"x1="+table)

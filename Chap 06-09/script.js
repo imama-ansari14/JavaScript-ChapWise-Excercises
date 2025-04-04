d4c0d2b6d6bb403c6ffa903a450aedc5ac4e2cc9
Imama Ansari <ansariimama@yahoo.com>
2025-04-04 21:03:41 +0500

    Question no 1 of chap 6 is in process...

--- Chap 06-09/script.js
+++ Chap 06-09/script.js
@@ -1,1 +1,9 @@
 // QUESTION NO 1
+let a =10
+let Result= a++
+console.log(Result);
+
+document.write("<p>Result:</p>")
+document.write("<p>The value of a is:"+ a + "</p> <hr>")
+document.write("<p>The value of ++a is:"+  + "</p> ")
+document.write("<p>Now the value of a is:"+  + "</p> <hr>")

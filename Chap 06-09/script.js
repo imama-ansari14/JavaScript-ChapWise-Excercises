26fd87b65bcca1dc4bd257ed95456337eb98471f
Imama Ansari <ansariimama@yahoo.com>
2025-04-04 21:18:32 +0500

    Question no 1 is Completed

--- Chap 06-09/script.js
+++ Chap 06-09/script.js
@@ -1,9 +1,20 @@
 // QUESTION NO 1
 let a =10
-let Result= a++
-console.log(Result);
 
 document.write("<p>Result:</p>")
 document.write("<p>The value of a is:"+ a + "</p> <hr>")
-document.write("<p>The value of ++a is:"+  + "</p> ")
-document.write("<p>Now the value of a is:"+  + "</p> <hr>")
+// preInc
+document.write("<p>The value of ++a is:"+  ++a + "</p> ")
+document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// postInc
+document.write("<p>The value of a++ is:"+  a++ + "</p> ")
+document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// preDec
+document.write("<p>The value of --a is:"+  --a + "</p> ")
+document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// postDec
+document.write("<p>The value of a-- is:"+  a-- + "</p> ")
+document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+
+// QUESTION NO 2
+

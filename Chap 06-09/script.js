f486a49289e985e4189c0376bbb79cd482b03914
Imama Ansari <ansariimama@yahoo.com>
2025-04-05 01:25:03 +0500

    Question no 3 is Done

--- Chap 06-09/script.js
+++ Chap 06-09/script.js
@@ -1,20 +1,32 @@
 // QUESTION NO 1
-let a =10
+// let a =10
 
-document.write("<p>Result:</p>")
-document.write("<p>The value of a is:"+ a + "</p> <hr>")
-// preInc
-document.write("<p>The value of ++a is:"+  ++a + "</p> ")
-document.write("<p>Now the value of a is:"+ a + "</p> <br>")
-// postInc
-document.write("<p>The value of a++ is:"+  a++ + "</p> ")
-document.write("<p>Now the value of a is:"+ a + "</p> <br>")
-// preDec
-document.write("<p>The value of --a is:"+  --a + "</p> ")
-document.write("<p>Now the value of a is:"+ a + "</p> <br>")
-// postDec
-document.write("<p>The value of a-- is:"+  a-- + "</p> ")
-document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// document.write("<p>Result:</p>")
+// document.write("<p>The value of a is:"+ a + "</p> <hr>")
+// // preInc
+// document.write("<p>The value of ++a is:"+  ++a + "</p> ")
+// document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// // postInc
+// document.write("<p>The value of a++ is:"+  a++ + "</p> ")
+// document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// // preDec
+// document.write("<p>The value of --a is:"+  --a + "</p> ")
+// document.write("<p>Now the value of a is:"+ a + "</p> <br>")
+// // postDec
+// document.write("<p>The value of a-- is:"+  a-- + "</p> ")
+// document.write("<p>Now the value of a is:"+ a + "</p> <br>")
 
 // QUESTION NO 2
+// var a =2
+// var b =1
+// var result= --a - --b + ++b + b-- 
+
+// document.write("a is "+a +"<br>")
+// document.write("b is "+b+"<br>")
+// document.write("Result is "+result)
+
+// QUESTION NO 3
+// let userInp= prompt('Enter your name')
+// alert("Hello, "+ userInp +"!");
+
 

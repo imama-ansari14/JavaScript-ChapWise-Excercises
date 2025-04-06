c3cc79e5ce6e31a2679f4279ddb1f31773536188
Imama Ansari <ansariimama@yahoo.com>
2025-04-06 23:53:39 +0500

    Question no 5 is in process ...

--- Chap 06-09/script.js
+++ Chap 06-09/script.js
@@ -19,7 +19,7 @@
 // QUESTION NO 2
 // var a =2
 // var b =1
-// var result= --a - --b + ++b + b-- 
+// var result= --a - --b + ++b + b--
 
 // document.write("a is "+a +"<br>")
 // document.write("b is "+b+"<br>")
@@ -29,4 +29,18 @@
 // let userInp= prompt('Enter your name')
 // alert("Hello, "+ userInp +"!");
 
+// QUESTION NO 5
+let num = prompt("Enter your frav number for multiplication table:");
+// for (let i = 1 ; i < 11 ; i++) {
+//     document.write(num +"X" + i + "=" + num*i + "<br>")
 
+// }
+if (num) {
+  for (let i = 1; i < 11; i++) {
+    document.write(num + "X" + i + "=" + num * i + "<br>");
+  }
+} else {
+  for (let i = 1; (i = 5); i++) {
+    console.log(5 + "X" + i + "=" + 5 * i + "<br>");
+  }
+}

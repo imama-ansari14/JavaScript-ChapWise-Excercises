c1f0632b36517bb8285af3fb4ae45658f72605c8
Imama Ansari <ansariimama@yahoo.com>
2025-04-08 23:52:08 +0500

    Question 6 is in process..

--- Chap 06-09/script.js
+++ Chap 06-09/script.js
@@ -30,13 +30,29 @@
 // alert("Hello, "+ userInp +"!");
 
 // QUESTION NO 5
-let num = prompt("Enter your frav number for multiplication table:");
-if (num) {
-  for (let i = 1; i < 11; i++) {
-    document.write(num + "X" + i + "=" + num * i + "<br>");
-  }
-} else {
-  for (let i = 1;i<=10 ; i++) {
-    document.write(5 + "X" + i + "=" + 5 * i + "<br>");
-  }
-}
+// let num = prompt("Enter your frav number for multiplication table:");
+// if (num) {
+//   for (let i = 1; i < 11; i++) {
+//     document.write(num + "X" + i + "=" + num * i + "<br>");
+//   }
+// } else {
+//   for (let i = 1;i<=10 ; i++) {
+//     document.write(5 + "X" + i + "=" + 5 * i + "<br>");
+//   }
+// }
+
+// QUESTION NO 6
+let SubOne= prompt("which subjects are  you studying?")
+let obtOne = +prompt("Enter obtained marks for " + SubOne);
+let SubTwo= prompt("Tell me your another subject")
+let obtTwo = +prompt("Enter obtained marks for " + SubTwo);
+let SubThree= prompt("Tell me your another subject")
+let obtThree = +prompt("Enter obtained marks for " + SubThree);
+tMarks = 100;
+
+document.write(
+  "<table border='1' cellspacing='0' cellpadding='10'><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>"
+);
+document.write("<tr><td>"+ SubOne+ "</td> <td>" +tMarks+"</td><td>" +obtOne+ "</td><td>" +obtOne+ " %</td></tr>")
+
+document.write("</table>")

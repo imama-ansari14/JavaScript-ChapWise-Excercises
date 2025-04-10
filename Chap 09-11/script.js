effbfba198f6359254eeefc446a36f1fc40eacad
Imama Ansari <ansariimama@yahoo.com>
2025-04-10 22:27:11 +0500

    Question no 1 of Chap 9 is done

---
+++ Chap 09-11/script.js
@@ -0,0 +1,10 @@
+// QUESTION NO 01
+// let city= prompt ("Enter your city name")
+// if (city=="karachi") {
+//     alert("Welcome to the city of lights")
+// } else{
+//     alert("Welcome to the " + city)
+// }
+
+// QUESTION NO 02
+

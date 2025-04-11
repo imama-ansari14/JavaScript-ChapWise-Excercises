29bdb33ec3ac10c94ed5484f853a6c493b1fadc2
Imama Ansari <ansariimama@yahoo.com>
2025-04-11 21:24:37 +0500

    Ques 3 is Done

--- Chap 09-11/script.js
+++ Chap 09-11/script.js
@@ -7,11 +7,24 @@
 // }
 
 // QUESTION NO 02
-let gender = prompt("Tell me your gender please!");
-if (gender === "male") {
-  alert("Good Morning Sir");
-} else if (gender === "female") {
-  alert("Good Morning Maam");
-} else {
-  alert("Good Morning");
-}
+// let gender = prompt("Tell me your gender please!");
+// if (gender === "male") {
+//   alert("Good Morning Sir");
+// } else if (gender === "female") {
+//   alert("Good Morning Maam");
+// } else {
+//   alert("Good Morning");
+// }
+
+// QUESTION NO 03
+// let trafficColor = prompt("Enter the color of the traffic signal:")
+// if (trafficColor === "red") {
+//       alert("Must Stop");
+//     } else if (trafficColor ==="yellow") {
+//       alert("Ready to move");
+//     } else if (trafficColor === "green") {
+//       alert("Move now");
+//     }else {
+//         alert("Invalid color entered");
+//     }
+

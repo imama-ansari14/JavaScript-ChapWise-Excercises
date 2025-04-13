76a45eb94a7fe599479585d06392c3820d58e898
Imama Ansari <ansariimama@yahoo.com>
2025-04-13 17:07:45 +0500

    Q5 is in process...

--- Chap 09-11/script.js
+++ Chap 09-11/script.js
@@ -37,7 +37,31 @@
 // }
 
 // QUESTION NO 05
-var a =4
+// a)
+var a = 4;
 if (++a === 5) {
-    alert("given condition for variable a is true")
+  alert("given condition for variable a is true");
 }
+// OUTPUT : given condition for variable a is true
+// b)
+var a = 82;
+if (b++ === 83) {
+  alert("given condition for variable b is true");
+}
+// OUTPUT : No alert will be shown
+// c)
+var c = 12;
+if (c++ === 13) {
+  alert("condition 1 is true");
+}
+if (c === 13) {
+  alert("condition 2 is true");
+}
+if (++c < 14) {
+  alert("condition 3 is true");
+}
+if (c === 14) {
+  alert("condition 4 is true");
+}
+// OUTPUT:
+

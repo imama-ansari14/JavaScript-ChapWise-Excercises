d4ed2c04023ab6f616a76715a8f992a38059baa3
Imama Ansari <ansariimama@yahoo.com>
2025-05-25 05:34:02 +0500

    Q3 is solved

--- Chap 12-13/script.js
+++ Chap 12-13/script.js
@@ -1,12 +1,22 @@
 
 // QUESTION NO 2
-let input1 = +prompt("Enter the first number of your choice");
-let input2 = +prompt("Enter the second number of your choice");
+// let input1 = +prompt("Enter the first number of your choice");
+// let input2 = +prompt("Enter the second number of your choice");
 
-if (input1 > input2) {
-  document.write("input 1 is big");
-} else if (input2 > input1) {
-     document.write("input 2 is big");
-} else if (input1 === input2) {
-  document.write("both are same");
+// if (input1 > input2) {
+//   document.write("input 1 is big");
+// } else if (input2 > input1) {
+//      document.write("input 2 is big");
+// } else if (input1 === input2) {
+//   document.write("both are same");
+// }
+
+// QUESTION NO 3
+let inpNum= +prompt("Enter any number of your choice")
+if (inpNum>0) {
+    document.write("The number is Positive")
+}else if(inpNum<0){
+document.write("The number is Negative")
+} else {
+    document.write("The number is Zero")
 }

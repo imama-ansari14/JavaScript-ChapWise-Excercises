f397bbdaea6907dbc8050d6a171985fbe4710fe0
Imama Ansari <ansariimama@yahoo.com>
2025-05-26 18:48:53 +0500

    Q4 is solved

--- Chap 12-13/script.js
+++ Chap 12-13/script.js
@@ -1,4 +1,3 @@
-
 // QUESTION NO 2
 // let input1 = +prompt("Enter the first number of your choice");
 // let input2 = +prompt("Enter the second number of your choice");
@@ -12,11 +11,23 @@
 // }
 
 // QUESTION NO 3
-let inpNum= +prompt("Enter any number of your choice")
-if (inpNum>0) {
-    document.write("The number is Positive")
-}else if(inpNum<0){
-document.write("The number is Negative")
+// let inpNum= +prompt("Enter any number of your choice")
+// if (inpNum>0) {
+//     document.write("The number is Positive")
+// }else if(inpNum<0){
+// document.write("The number is Negative")
+// } else {
+//     document.write("The number is Zero")
+// }
+
+// QUESTION NO 4
+let inpCharac = prompt("Enter any alphabet of your choice (One Letter)");
+if (inpCharac === "a" ||
+  inpCharac === "e" ||
+  inpCharac === "i" ||
+  inpCharac === "o" ||
+  inpCharac === "u") {
+  document.write("The alphabet is vowel");
 } else {
-    document.write("The number is Zero")
+  document.write("The alphabet is consonant");
 }

03e933b8888f391803f45fb80a3151d343500cae
Imama Ansari <ansariimama@yahoo.com>
2025-06-23 01:02:33 +0500

    Q2 is done

--- Es6 Part 1/app.js
+++ Es6 Part 1/app.js
@@ -8,5 +8,10 @@ if (1 === 1) {
   console.log(myname);
 }
 console.log(broName);
-console.log(myname)  //myname is not defined
-// We can access the variable declaring with var outside the block scope but cant access the variable declaring with let 
+// console.log(myname); //myname is not defined
+// We can access the variable declaring with var outside the block scope but can't access the variable declaring with let.
+
+// QUESTION NO 2
+const age = 18;
+// age= 17
+// we cant reassign const variable because JavaScript take it as constant value like pie

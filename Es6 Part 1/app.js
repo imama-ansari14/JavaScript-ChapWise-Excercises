4c1a049492048bf71f640d00411d9d1112a27047
Imama Ansari <ansariimama@yahoo.com>
2025-06-23 00:24:06 +0500

    Q1 of Es6 is done

--- Es6 Part 1/app.js
+++ Es6 Part 1/app.js
@@ -1,1 +1,12 @@
+//VARIABLE SCOPING (VAR/LET/CONST)
 // QUESTION NO 1
+if (1 === 1) {
+  var broName = "arham";
+  console.log(broName);
+
+  let myname = "imama";
+  console.log(myname);
+}
+console.log(broName);
+console.log(myname)  //myname is not defined
+// We can access the variable declaring with var outside the block scope but cant access the variable declaring with let 

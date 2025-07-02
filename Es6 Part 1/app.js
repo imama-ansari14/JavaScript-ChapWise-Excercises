5057b0d8683367db0c1f5f42591ea71072da1279
Imama Ansari <ansariimama@yahoo.com>
2025-07-03 02:48:36 +0500

    VARIABLE SCOPING Chap is done

--- Es6 Part 1/app.js
+++ Es6 Part 1/app.js
@@ -12,17 +12,17 @@ console.log(broName);
 // We can access the variable declaring with var outside the block scope but can't access the variable declaring with let.
 
 // QUESTION NO 2
+//Declare a variable age using const and try to reassign its value. What happens? Explain why.
+
 const age = 18;
 // age= 17
 // we cant reassign const variable because JavaScript take it as constant value like pie
 
 // QUESTION NO 3 (ChatGpt)
 // Declare a global variable using var and access it inside a function.
-var global ="globally declared"
+var global = "globally declared";
 function name(param) {
- console.log(global);
- 
+  console.log(global);
 }
-name()
+name();
 
-// QUESTION NO 4

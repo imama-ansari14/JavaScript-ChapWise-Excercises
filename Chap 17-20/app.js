0c883a166320eee9a706d8bcb6c86c2a26c94e4f
Imama Ansari <ansariimama@yahoo.com>
2025-07-18 02:17:56 +0500

    Q1 and 2 is Done

--- Chap 17-20/app.js
+++ Chap 17-20/app.js
@@ -1,3 +1,13 @@
 // QUESTION NO 1
+let matrix=[ [],[],[] ]
 
- 
+// QUESTION NO 2
+let mulDimen = [
+  [0, 1, 2, 3],
+  [1, 0, 1, 2],
+  [2, 1, 0, 1]
+];
+document.write(mulDimen[0],"<br>")
+document.write(mulDimen[1],"<br>")
+document.write(mulDimen[2],"<br>")
+

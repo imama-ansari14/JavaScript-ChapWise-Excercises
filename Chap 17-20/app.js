45bb29c65db771c0fe02f6dcb7b586822a47a693
Imama Ansari <ansariimama@yahoo.com>
2025-07-18 02:25:06 +0500

    Q3 is done

--- Chap 17-20/app.js
+++ Chap 17-20/app.js
@@ -11,3 +11,14 @@ document.write(mulDimen[0],"<br>")
 document.write(mulDimen[1],"<br>")
 document.write(mulDimen[2],"<br>")
 
+// QUESTION NO 3
+for (let i = 1; i <= 10; i++) {
+
+    document.write(i, "<br>")
+}
+
+// QUESTION NO 4
+
+
+
+

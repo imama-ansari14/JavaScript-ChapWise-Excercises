7ff33de747e9461caf57ada5fe434f7ef26cb93f
Imama Ansari <ansariimama@yahoo.com>
2025-08-21 00:59:08 +0500

    Q2 is done

--- Chap 21-25/app.js
+++ Chap 21-25/app.js
@@ -1,6 +1,12 @@
 //QUESTION NO 1
-let firstName= prompt("Enter your first name")
-let lastName= prompt("Enter your last name")
-let fullName= `${firstName} ${lastName}`
-alert(`Helloo! ${fullName}`)
+// let firstName= prompt("Enter your first name")
+// let lastName= prompt("Enter your last name")
+// let fullName= `${firstName} ${lastName}`
+// alert(`Helloo! ${fullName}`)
+
+//QUESTION NO 2
+let mobPhone= prompt("Enter your frav Mobile Phone Model")
+let length=mobPhone.length
+document.write(`My Favorite phone is: ${mobPhone}</br>`)
+document.write(`Length of string: ${length}`)
 

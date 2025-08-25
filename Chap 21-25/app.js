a4b300dc0a2f0b86346b0e3d02bd335d2d59bb3b
Imama Ansari <ansariimama@yahoo.com>
2025-08-26 00:47:18 +0500

    Q6 is done

--- Chap 21-25/app.js
+++ Chap 21-25/app.js
@@ -29,10 +29,11 @@ document.write(`String: ${nationality}</br>`)
 document.write(`Character at index 3: ${find}</br></br>`)
 
 //QUESTION NO 6
-// let firstName= prompt("Enter your first name")
-// let lastName= prompt("Enter your last name")
-// let fullName= `${firstName} ${lastName}`
-// alert(`Helloo! ${fullName}`)
+let firstName= prompt("Enter your first name")
+let lastName= prompt("Enter your last name")
+let fullName= firstName.concat(" "+ lastName)
+document.write(`${fullName}</br></br>`)
+
 
 //QUESTION NO 7
 let city = "Hyderabad";
@@ -43,8 +44,15 @@ document.write(`After replacement: ${newCity}</br></br>`)
 //QUESTION NO 8
 var message = "Ali and sami are best friends. They play cricket and football together."
 let newMess=message.replaceAll("and","&")
-document.write(`City: ${message}</br>`)
-document.write(`After replacement: ${newMess}</br>`)
+document.write(`Message: ${message}</br>`)
+document.write(`After replacement: ${newMess}</br></br>`)
+
+//QUESTION NO 9
+
+document.write(`Value: ${city}</br>`)
+document.write(`Type: ${newCity}</br>`)
+document.write(`Value: ${city}</br>`)
+document.write(`Type: ${newCity}</br></br>`)
 
 
 

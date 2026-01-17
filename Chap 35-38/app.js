202acc0695618acaafbed28a460af842de9b950a
Imama Ansari <ansariimama@yahoo.com>
2026-01-17 16:39:34 +0500

    solving question

---
+++ Chap 35-38/app.js
@@ -0,0 +1,11 @@
+//--------- CHAP NO 35-38----------
+
+// QUESTION NO 1
+
+
+// QUESTION NO 1
+let firstName= prompt("Enter your first name");
+let secondName= prompt("Enter your last name");
+function name(params) {
+    
+}

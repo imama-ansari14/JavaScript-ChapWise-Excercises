eab6ce0d5a0a4822de4ea6ae21ef0ff759cece16
Imama Ansari <ansariimama@yahoo.com>
2026-01-18 17:16:52 +0500

    Quest 1 and 2 is solved

--- Chap 35-38/app.js
+++ Chap 35-38/app.js
@@ -1,11 +1,19 @@
 //--------- CHAP NO 35-38----------
 
 // QUESTION NO 1
+function date_time() {
+    let date = new Date();
+    document.write(date + "<br/>");
+}
+date_time();
 
 
 // QUESTION NO 1
-let firstName= prompt("Enter your first name");
-let secondName= prompt("Enter your last name");
-function name(params) {
-    
+function greet(firstName, lastName) {
+    document.write(`Hello! ${firstName} ${lastName}, Welcome to our website`);
 }
+
+let firstName = prompt("Enter your first name");
+let secondName = prompt("Enter your last name");
+
+greet(firstName, secondName);

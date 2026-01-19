4639ee36356d72b30a9527e253ff215ab436c597
Imama Ansari <ansariimama@yahoo.com>
2026-01-19 18:30:41 +0500

    solve ques no 3

--- Chap 35-38/app.js
+++ Chap 35-38/app.js
@@ -8,12 +8,22 @@ function date_time() {
 date_time();
 
 
-// QUESTION NO 1
+// QUESTION NO 2
 function greet(firstName, lastName) {
-    document.write(`Hello! ${firstName} ${lastName}, Welcome to our website`);
+    document.write(`Hello! ${firstName} ${lastName}, Welcome to our website <br/>`);
 }
 
 let firstName = prompt("Enter your first name");
 let secondName = prompt("Enter your last name");
 
 greet(firstName, secondName);
+
+// QUESTION NO 3
+let num1 = +prompt("Enter first number");
+let num2 = +prompt("Enter second number");
+function add() {
+    document.write(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
+}
+add();
+
+// QUESTION NO 4
